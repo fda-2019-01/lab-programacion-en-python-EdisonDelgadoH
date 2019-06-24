d0d256d444b0d8b508ff4c598567fc9dbb8ebab6
EdisonDelgadoH <50671043+EdisonDelgadoH@users.noreply.github.com>
2019-06-24 18:36:27 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,3 +1,11 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
+data=open('data.csv', 'r').readlines()
+data
+cont=0
+i=0
+col=[column[2] for column in data]
+for i in col:
+  cont=cont+int(i)
+print(cont)
