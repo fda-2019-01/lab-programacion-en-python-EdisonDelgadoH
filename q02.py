3c084575c5298e93fa8e2662bbd5568fbd33840c
EdisonDelgadoH <50671043+EdisonDelgadoH@users.noreply.github.com>
2019-06-24 22:17:37 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -8,3 +8,14 @@
 ## D,6
 ## E,14
 ##
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas
+%matplotlib inline
+data=open('data.csv', 'r').readlines()
+data=[column[:-1].split("\t") for column in data]
+fila=[column[0] for column in data]
+letra=sorted(set(fila))
+for h in letra:
+  print("{},{}".format(h, fila.count(h)))
