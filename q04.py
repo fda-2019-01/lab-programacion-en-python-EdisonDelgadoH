527eb3dfe42a8f7c0fa9c2ac07167d282d924242
EdisonDelgadoH <50671043+EdisonDelgadoH@users.noreply.github.com>
2019-07-06 17:55:07 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -14,3 +14,12 @@
 ## 11,2
 ## 12,3
 ##
+data=open('data.csv', 'r').readlines()
+data=[line.replace('\t', ';') for line in data]
+#data=[line[:1] for line in data]
+data = [line.split(';')[2] for line in data]
+h=0
+fila=[str(column[5])+str(column[6]) for column in data]
+fil=sorted(set(fila))
+for h in fil:
+  print("{},{}".format(h, fila.count(h)))
